270100102e4680cba7e3c641089b2625e9a72e0a
berkincetin <bcetin14@gmail.com>
2020-04-11 11:44:06 -0400

    finished memory game

--- app.js
+++ app.js
@@ -51,18 +51,65 @@ document.addEventListener('DOMContentLoaded',() =>{
         },
     ]
 
+    cardArray.sort(() => 0.5 - Math.random());
+
     const grid = document.querySelector('.memory-game-grid');
+    const resultDisplay = document.querySelector('#memory-game-result');
+    var cardsChosen = [];
+    var cardsChosenId = [];
+    var cardsWon = [];
 
     //create board
     function createBoard() {
         for (let i =0; i <cardArray.length; i++){
             var card = document.createElement('img');
             card.setAttribute('src', 'images/blank.png');
-            card.setAttribute('card-data-id',i);
-            //card.addEventListener('click', flipcard);
+            card.setAttribute('data-id',i);
+            card.classList.add("memory-game-card");
+            card.addEventListener('click', flipCard);
             grid.appendChild(card);
         }
     }
 
+    //check for matches
+    function checkForMatch() {
+        var cards=document.querySelectorAll('.memory-game-card');
+        console.log(cards);
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+
+        if (cardsChosen[0] === cardsChosen[1]) {
+            cards[optionOneId].setAttribute('src','images/white.png');
+            cards[optionTwoId].setAttribute('src','images/white.png');
+            cardsWon.push(cardsChosen);
+            console.log(cardsChosen);
+            console.log(cardsWon);
+
+        } else {
+            cards[optionOneId].setAttribute('src','images/blank.png');
+            cards[optionTwoId].setAttribute('src','images/blank.png');
+        }
+        cardsChosen = [];
+        cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = "Congrats! You found them!";
+        }
+    }
+
+    //flip card
+
+    function flipCard() {
+        var cardId = this.getAttribute('data-id');
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+
+    }
+
     createBoard();
+
 });
