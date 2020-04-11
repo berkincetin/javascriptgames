debcdde8aae8a31fd70eed28eedda4d89be7caab
berkincetin <bcetin14@gmail.com>
2020-04-11 11:10:12 -0400

    Added styling to layout

---
+++ app.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded',() =>{
+   //Create card array of options 
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+    ]
+
+    const grid = document.querySelector('.memory-game-grid');
+
+    //create board
+    function createBoard() {
+        for (let i =0; i <cardArray.length; i++){
+            var card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('card-data-id',i);
+            //card.addEventListener('click', flipcard);
+            grid.appendChild(card);
+        }
+    }
+
+    createBoard();
+});
